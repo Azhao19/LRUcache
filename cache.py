7989b4f7d79a981a5050d8a976c0c3b50b8f7bf0
Alan Zhao <asz2115@columbia.edu>
2021-08-06 12:54:55 -0600

    main executable

---
+++ cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+# flags:
+# -c create
+# -r remove; -f force remove
+# -u list of URL,cache
+# -o open
+
+import os
+import sys
+from LRUCache import *
+
+list_of_LRU_caches = {}
+for x in sys.argv[1:]:
+    # open routine
+    # python script
+
+    
