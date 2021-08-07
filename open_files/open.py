84d8aa26186e753d56e3b6a2f1805f1a4c4508c8
Alan Zhao <asz2115@columbia.edu>
2021-08-06 23:42:31 -0600

    allowed the program to modify metadata

--- open_files/open.py
+++ open_files/open.py
@@ -14,38 +14,46 @@ if (not os.path.exists(os.path.join(Path("."), "cache"))):
     exit(1)
 
 p = Path("./cache")
+
+# input validation
+for test in sys.argv[1:]:
+    test = test.split(",")
+    if (not len(test) == 2):
+        print("Invalid input to open.")
+        exit(1)
+    if (not os.path.exists(os.path.join(p, "%s/%s"% (test[1], test[0])))):
+        print("Not all files exist.")
+        exit(1)
+ 
 for x in sys.argv[1:]:
     x = x.split(",")
-    if (not len(x) == 2):
-        print("Files to open must be presented as a list of items of the form file,cache")
+    
+    p_file = os.path.join(p, "%s/%s" % (x[1], x[0]))
+    y = round(os.stat(p_file).st_size / (1024 * 1024), 8)
+    pid = os.fork()
+    if (pid < 0):
+        print("Fork failed.")
         exit(1)
+    elif (pid == 0):
+        os.execlp("vim", "vim", p_file)
     else:
-        p_file = os.path.join(p, "%s/%s" % (x[1], x[0]))
-        y = round(os.stat(p_file).st_size / (1024 * 1024), 8)
-        pid = os.fork()
-        if (pid < 0):
-            print("Fork failed.")
-            exit(1)
-        elif (pid == 0):
-            os.execlp("vim", "vim", p_file)
-        else:
-            os.waitpid(pid, 0)
+        os.waitpid(pid, 0)
 
-            y = round(os.stat(p_file).st_size / (1024 * 1024), 8) - y
-            f = open(os.path.join(p, "%s/.size" % x[1]), "r")
-            lines = f.readlines()
-            z = float(lines[1]) + y
-            lines[1] = str(z)
-            f = open(os.path.join(p, "%s/.size" % x[1]), "w")
-            f.writelines(lines)
-            f.close()
+        y = round(os.stat(p_file).st_size / (1024 * 1024), 8) - y
+        f = open(os.path.join(p, "%s/.size" % x[1]), "r")
+        lines = f.readlines()
+        z = float(lines[1]) + y
+        lines[1] = str(z)
+        f = open(os.path.join(p, "%s/.size" % x[1]), "w")
+        f.writelines(lines)
+        f.close()
 
-            g = open(os.path.join(p, "%s/.files" % x[1]), "a")
-            g.write(x[0])
-            g.close()
+        g = open(os.path.join(p, "%s/.files" % x[1]), "a")
+        g.write(x[0])
+        g.close()
 
-            # Deletion of the old entry corresponding to x[0] will be done by the main program
-            # which has access to the LRUCache data structures
+        # Deletion of the old entry corresponding to x[0] will be done by the main program
+        # which has access to the LRUCache data structures
 
 
 
